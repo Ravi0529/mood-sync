41fcbd3e96043f34987ffe2ba9b1d0b640b219c0
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-03-29 22:23:58 +0530

    prisma.ts update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,8 +4,6 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
