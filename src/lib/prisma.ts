5ccdcca3d6b8db8317138cb99a96d0544e2c3a25
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-03-29 21:49:13 +0530

    bug fixes(#7)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-const prisma: PrismaClient = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export default prisma;
